fe9c4f9dcd4854013f8e0ad52a6a1a4c39912f69
joaolmkormann <joaolmkormnn@gmail.com>
2025-11-29 15:02:27 -0300

    Adicionando os relatorios escritos

--- Proj Final/Elevator1.asm
+++ Proj Final/Elevator1.asm
@@ -46,7 +46,7 @@ Elevator1_process:
 	bne	$s7,-2, elevador1_RequestMovendo_descendo_buscando
 	lw	$t0, 8($t1)			# carregando o destino final do request
 	lw	$t2, Elevador1_andarDestino	# carregando o detino atual
-	ble	$t2, $t0, elevador1_ClearRequest	####era t2
+	ble	$t2, $t0, elevador1_ClearRequest 	####era t2
 	sw	$t0, Elevador1_andarDestino
 	j	elevador1_ClearRequest
 	
@@ -72,7 +72,7 @@ Elevator1_process:
 		beqz	$t0, exitElevator1
 	taMovendo1:
 		lw	$t1, Elevador1_passoTimer
-		addi 	$t1, $t1, 1000
+		addi 	$t1, $t1, 100
 		sw	$t1, Elevador1_passoTimer
 		bge	$t1, 4000, passaAndar1
 		j	exitElevator1
